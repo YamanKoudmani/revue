eed466b927b00f2b74c2728d1029ddac957d4983
tguyenphananh <tnguyenphananh@knox.edu>
2021-04-18 15:08:00 -0500

    fix

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/views/Home";
@@ -72,72 +71,3 @@ const router = createRouter({
 });
 
 export default router;
-=======
-import { createRouter, createWebHistory } from 'vue-router';
-
-import Home from '@/views/Home'
-
-// GUIDE: This file defines urls for the frontend
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/Search',
-    name: 'Search-Page',
-    component: () => import( '@/views/SearchPage')  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "login" */ '@/views/Login')
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: () => import(/* webpackChunkName: "signup" */ '@/views/SignUp')
-  },
-  {
-    path: '/s/:name',
-    name: 'Subvue',
-    component: () => import(/* webpackChunkName: "subvue" */ '@/views/Subvue')
-  },
-  {
-    path: '/s/:subvuePermalink/:id',
-    name: 'Post',
-    component: () => import(/* webpackChunkName: "post" */ '@/views/Post')
-  },
-  {
-    path: '/u/:username',
-    name: 'User',
-    component: () => import(/* webpackChunkName: "user" */ '@/views/User')
-  },
-  {
-    path: '/create',
-    name: 'CreatePost',
-    component: () => import(/* webpackChunkName: "create" */ '@/views/CreatePost')
-  },
-  {
-    path: '/create/subvue',
-    name: 'CreateSubvue',
-    component: () => import(/* webpackChunkName: "createsubvue" */ '@/views/CreateSubvue')
-  },
-  {
-
-
-    path: '/service',
-    name: 'service',
-    component: () => import(/* webpackChunkName: "services" */ '@/views/service')
-  },
-
-
-]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
-
-export default router
->>>>>>> development
